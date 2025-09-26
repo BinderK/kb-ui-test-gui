49c5aa14887e850bc8eb6e44a80ae1c060b989e3
Kevin <kevinbinder92@gmail.com>
2025-09-26 15:04:18 -0400

    Modified: Add debug profile for React to launch.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,8 +13,13 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    sourcemap: true,
   },
   server: {
     port: 3000,
+    sourcemapIgnoreList: false,
+  },
+  css: {
+    devSourcemap: true,
   },
 })
